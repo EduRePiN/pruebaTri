3a1af4dbc4ed3d852d7f462118564de25a3d9df6
Karen Pigni <64334155+Karen-Pigni@users.noreply.github.com>
2023-10-21 22:19:00 -0300

    se agrega CRUD

--- app/src/main/java/com/infowithvijay/triviaquizapp2/MainActivity.java
+++ app/src/main/java/com/infowithvijay/triviaquizapp2/MainActivity.java
@@ -6,36 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btniniciarsesion;
-    Button btnregistro;
-
-    // declare una variable indicando el tipo de control
-
+    EditText editText_name,editText_email;
+    Button button_add,button_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btniniciarsesion = findViewById(R.id.butn_iniciarsesionm);
-        btnregistro = findViewById(R.id.butn_iniciarsesionm2);
-        btniniciarsesion.setOnClickListener(new View.OnClickListener() {
+        editText_name = findViewById(R.id.edittext_name);
+        editText_email = findViewById(R.id.edittext_email);
+        button_add = findViewById(R.id.button_add);
+        button_view = findViewById(R.id.button_view);
+
+
+        button_add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this , SignUpActivity.class);
+            public void onClick(View v) {
+                String stringName = editText_name.getText().toString();
+                String stringEmail = editText_email.getText().toString();
+
+                if (stringName.length() <= 0 || stringEmail.length() <= 0) {
+                    Toast.makeText(MainActivity.this, "Enter All Data", Toast.LENGTH_SHORT).show();
+                } else {
+                    DatabaseHelperClass databaseHelperClass = new DatabaseHelperClass(MainActivity.this);
+                    EmployeeModelClass employeeModelClass = new EmployeeModelClass(stringName, stringEmail);
+                    databaseHelperClass.addEmployee(employeeModelClass);
+                    Toast.makeText(MainActivity.this, "Add Employee Successfully", Toast.LENGTH_SHORT).show();
+                    finish();
+                    startActivity(getIntent());
+                }
+            }
+        });
+
+
+        button_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewEmployeeActivity.class);
                 startActivity(intent);
             }
         });
-        btnregistro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this , SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
 
     //codigo para ir a otra pagina indicando a donde quiero ir!//
-}
+}}
